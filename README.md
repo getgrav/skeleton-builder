c7312840d1fa91a57cbf4b0c391ba097fcdd36da
Djamil Legato <djamil@djamil.it>
2021-02-19 23:09:51 -0800

    More progress + README udpates

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # skeleton-builder
 GitHub Action to create fully functional skeleton packages that include all the necessary dependencies
+
+## Development / Debugging
+This `action` comes with a built-in workflow that helps with development. By using [act](https://github.com/nektos/act), it can be entirely run locally, without the need of pushing to GitHub.
+
+To run the action locally, open your terminal and `cd` to the location of this file, then simply run `act`.
+
+Act will run the built-in workflow which is set to point to the `entrypoint.sh` file at this location. Any change applied to the file can be tested by re-running `act`.
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,20 @@
 
 set -eu
 
-$REPOSITORY_NAME=$(echo "$GITHUB_REPOSITORY" | awk -F / '{print $2}' | sed -e "s/:refs//")
-$VERSION=$GITHUB_REF
+mkdir /dist
 
+REPOSITORY_NAME=$(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $2}' | sed -e "s/:refs//")
 
-echo "${REPOSITORY_NAME} ${VERSION} ${INPUT_VERSION} ${INPUT_ADMINPLUGIN}"
+echo "${REPOSITORY_NAME} ${INPUT_VERSION} ${INPUT_ADMINPLUGIN}"
 
-curl -L https://getgrav.org/download/core/grav/${INPUT_VERSION} > /grav.zip;
+# Download Grav @ specific version and get it ready for the Skeleton
+cd /dist
+curl --write-out '%{http_code}' --silent --location https://getgrav.org/download/core/grav/${INPUT_VERSION} > grav.zip;
+unzip -q grav.zip
+rm -rf grav.zip
+cd grav
+bin/grav install
+ls -la .
 
-ls .
-ls -la /
+ls -la $GITHUB_WORKSPACE
 # zip -q -x "*.git/*" -x "*.github/workflows/*" -x *yarn.lock* -x *.gitignore* -x *.editorconfig* -x *.DS_Store* -x *hebe.json* -x *.dependencies* -x *.travis.yml* -r $1 $2
