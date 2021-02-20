63a5d0589b6bee3de013eba17a2507a04427d8b0
Djamil Legato <djamil@djamil.it>
2021-02-19 23:26:13 -0800

    More progress, create zips

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,29 @@
 
 set -eu
 
-mkdir /dist
+mkdir /dist /build
 
 REPOSITORY_NAME=$(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $2}' | sed -e "s/:refs//")
+VERSION=$GITHUB_REF
 
 echo "${REPOSITORY_NAME} ${INPUT_VERSION} ${INPUT_ADMINPLUGIN}"
 
 # Download Grav @ specific version and get it ready for the Skeleton
-cd /dist
+cd /build
 curl --write-out '%{http_code}' --silent --location https://getgrav.org/download/core/grav/${INPUT_VERSION} > grav.zip;
 unzip -q grav.zip
 rm -rf grav.zip
-cd grav
-#bin/grav install
-ls -la .
 
-ls -la $GITHUB_WORKSPACE
+cd grav
+
+rm -rf user
+cp -Rf $GITHUB_WORKSPACE user
+bin/grav install
+
+zip -q -x "*.git/*" -x "*.github/workflows/*" -x *yarn.lock* -x *.gitignore* -x *.editorconfig* -x *.DS_Store* -x *hebe.json* -x *.dependencies* -x *.travis.yml* -r "/dist/${REPOSITORY_NAME}-${VERSION}.zip" /build
+
+ls -la .
+ls -la /dist
+ls -la /build
+# ls -la $GITHUB_WORKSPACE
 # zip -q -x "*.git/*" -x "*.github/workflows/*" -x *yarn.lock* -x *.gitignore* -x *.editorconfig* -x *.DS_Store* -x *hebe.json* -x *.dependencies* -x *.travis.yml* -r $1 $2
