65f4cfc68869e36ac2f27b2626c11d1a1f8df463
Djamil Legato <djamil@djamil.it>
2021-02-22 10:42:14 -0800

    Make action quiet but support verbose mode as an option

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,32 +2,46 @@
 
 set -eu
 
+# Prepping files will reside in /dist
+# Output zip builds will reside in /build
 mkdir /dist /build
 
+# Variables prepping
 REPOSITORY_NAME=$(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $2}' | sed -e "s/:refs//")
 VERSION=$($GITHUB_REF | sed -e "s/refs\/heads\///")
+SILENT='-q'
+CURL_SILENT='--silent'
 
-echo "${REPOSITORY_NAME} ${INPUT_VERSION} ${INPUT_ADMINPLUGIN}"
+if $INPUT_VERBOSE; then
+    SILENT=
+    CURL_SILENT=
+fi
 
 # Download Grav @ specific version and get it ready for the Skeleton
 cd /build
-curl --write-out '%{http_code}' --silent --location https://getgrav.org/download/core/grav/${INPUT_VERSION} > grav.zip;
-unzip -q grav.zip
+curl --write-out '%{http_code}' ${CURL_SILENT} --location https://getgrav.org/download/core/grav/${INPUT_VERSION} > grav.zip;
+unzip ${SILENT} grav.zip
 rm -rf grav.zip
 
 cd grav
 
+# Install the skeleton user folder
 rm -rf user
 cp -Rf $GITHUB_WORKSPACE user
-bin/grav install
+bin/grav install ${SILENT}
 
-zip -q -x "*.git/*" -x "*.github/workflows/*" -x *yarn.lock* -x *.gitignore* -x *.editorconfig* -x *.DS_Store* -x *hebe.json* -x *.dependencies* -x *.travis.yml* -r "/dist/${REPOSITORY_NAME}-${VERSION}.zip" /build
+# Create a package of the skeleton (w/o admin)
+zip ${SILENT} -x "*.git/*" -x "*.github/workflows/*" -x *yarn.lock* -x *.gitignore* -x *.editorconfig* -x *.DS_Store* -x *hebe.json* -x *.dependencies* -x *.travis.yml* -r "/dist/${REPOSITORY_NAME}-${VERSION}.zip" /build
 
+# If option enabled, also create a package with admin included
 if $INPUT_ADMINPLUGIN; then
-    bin/gpm install admin -y
-    zip -q -x "*.git/*" -x "*.github/workflows/*" -x *yarn.lock* -x *.gitignore* -x *.editorconfig* -x *.DS_Store* -x *hebe.json* -x *.dependencies* -x *.travis.yml* -r "/dist/${REPOSITORY_NAME}+admin-${VERSION}.zip" /build
+    bin/gpm install admin -y ${SILENT}
+    zip ${SILENT} -x "*.git/*" -x "*.github/workflows/*" -x *yarn.lock* -x *.gitignore* -x *.editorconfig* -x *.DS_Store* -x *hebe.json* -x *.dependencies* -x *.travis.yml* -r "/dist/${REPOSITORY_NAME}+admin-${VERSION}.zip" /build
 fi
 
-ls -la .
-ls -la /dist
-ls -la /build
+# When in verbose mode, output a list of installed plugins/themes and final /build folder
+if $INPUT_VERBOSE; then
+    ls -la user/plugins
+    ls -la user/themes
+    ls -la /build
+fi
