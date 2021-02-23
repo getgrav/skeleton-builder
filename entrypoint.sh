41255e340db55fe0c28c7fa7daf105f489817362
Djamil Legato <djamil@djamil.it>
2021-02-22 21:52:20 -0800

    Make version variable supporting workflow dispatch

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,13 @@ set -eu
 # Output zip packages will reside in /dist
 mkdir /dist /build
 
+# Extrapolate version
+VERSION=${{ GITHUB_EVENT_INPUTS_TAG || GITHUB_REF }}
+echo "SKELETON_VERSION=${{ VERSION }}" >> $GITHUB_ENV
+VERSION=$(echo "${VERSION}" | sed -e "s/refs\/heads\///" | sed -e "s/refs\/tags\///")
+
 # Variables prepping
 REPOSITORY_NAME=$(echo "${GITHUB_REPOSITORY}" | awk -F / '{print $2}' | sed -e "s/:refs//")
-VERSION=$(echo "${GITHUB_REF}" | sed -e "s/refs\/heads\///" | sed -e "s/refs\/tags\///")
 SILENT='-q'
 CURL_SILENT='--silent'
 
